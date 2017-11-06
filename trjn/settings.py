20f7d44b6ae934ff17f9737169d07b4f4ad08e59
Romanov <romanovs@amazon.com>
2017-11-06 15:35:16 +0800

    adding templates back to config for admin.

--- trjn/settings.py
+++ trjn/settings.py
@@ -56,6 +56,21 @@ ROOT_URLCONF = 'trjn.urls'
 
 WSGI_APPLICATION = 'trjn.wsgi.application'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'librato_chart_sender/templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
