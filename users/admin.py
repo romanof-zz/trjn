f43a3fe143e3341a1ceff8ec1e2bd0a77ed0859b
Romanov <romanovs@amazon.com>
2017-11-20 19:16:10 +0700

    create nested admin for profiles and identities.

--- users/admin.py
+++ users/admin.py
@@ -3,14 +3,22 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from .models import (Identity, Profile)
+import nested_admin
 
-class ProfileAdmin(admin.ModelAdmin):
+class IdentityAdmin(nested_admin.NestedStackedInline):
+    model = Identity
+    classes = ('collapse',)
+    extra = 0
+
+class ProfileAdmin(nested_admin.NestedModelAdmin):
+    inlines = [IdentityAdmin]
     list_display = ('user', 'bio')
-    exclude = ('identities',)
 
-class IdentityAdmin(admin.ModelAdmin):
-    list_display = ('provider', 'external_id')
+    def get_queryset(self, request):
+        qs = super(ProfileAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=request.user)
 
 # Register your models here.
 admin.site.register(Profile, ProfileAdmin)
-admin.site.register(Identity, IdentityAdmin)
--- users/models.py
+++ users/models.py
@@ -4,9 +4,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    picture = models.CharField(max_length=1024)
-    bio = models.CharField(max_length=1024)
     user = models.ForeignKey(User, related_name="profile_user", on_delete=models.CASCADE)
+    bio = models.CharField(max_length=1024)
+    picture = models.CharField(max_length=1024)
 
     def __str__(self):
         return self.user.username
